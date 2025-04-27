7262a1ccffbe77db69d3d66671cd75cf276d4600
simtaejin <stj53@nate.com>
2025-04-27 16:11:30 +0900

    "DatabaseSeeder에 UserSeeder 호출 추가"

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,6 +24,7 @@ class DatabaseSeeder extends Seeder
 //            UserSeeder::class,
 //        ]);
         $this->call([
+            UserSeeder::class,
             ChannelSeeder::class,
         ]);
     }
