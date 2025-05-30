ed58b77f00328ca983af99dee1ceea7149c0f8db
simtaejin <stj53@nate.com>
2025-05-31 02:59:30 +0900

    "MongoDB 모델 수정: VideoStat 모델 네임스페이스 변경 및 컬렉션 필드의 datetime 변환 처리 추가"

--- app/Models/VideoStat.php
+++ app/Models/VideoStat.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Models;
+namespace App\Models\Mongo;
 
 use Jenssegers\Mongodb\Eloquent\Model;
 
@@ -10,8 +10,18 @@ class VideoStat extends Model
     protected $collection = 'video_stats';
 
     protected $fillable = [
-        'video_id', 'channel_id', 'collected_at',
-        'view_count', 'like_count', 'comment_count', 'created_at'
+        'video_id',
+        'channel_id',
+        'collected_at',
+        'view_count',
+        'like_count',
+        'comment_count',
+        'created_at',
+    ];
+
+    protected $casts = [
+        'collected_at' => 'datetime',
+        'created_at' => 'datetime',
     ];
 
     public $timestamps = false;
