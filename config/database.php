08ac573b47a39b82bb919da33f91705749d5446d
simtaejin <stj53@nate.com>
2025-05-31 03:18:16 +0900

    "MongoDB 설정 수정: 환경 변수 키 변경 (DB_* → MONGO_DB_*)"

--- config/database.php
+++ config/database.php
@@ -114,11 +114,11 @@ return [
 
         'mongodb' => [
             'driver' => 'mongodb',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', 27017),
-            'database' => env('DB_DATABASE'),
-            'username' => env('DB_USERNAME'),
-            'password' => env('DB_PASSWORD'),
+            'host' => env('MONGO_DB_HOST', '127.0.0.1'),
+            'port' => env('MONGO_DB_PORT', 27017),
+            'database' => env('MONGO_DB_DATABASE'),
+            'username' => env('MONGO_DB_USERNAME'),
+            'password' => env('MONGO_DB_PASSWORD'),
             'options' => [
                 'database' => env('DB_AUTHENTICATION_DATABASE', env('DB_DATABASE')), // 인증용 DB 명시
             ],
